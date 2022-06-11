59f01e5d87329864ffbad25d33193bb3cd8c1b97
Ahmad Raza <ahmadzaheer506@gmail.com>
2022-06-12 00:47:49 +0500

    first commit

---
+++ src/build/precache-manifest.7bffe3d4d917938340d96d5e5031caf7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2fb26e57681765110c79",
+    "url": "/static/css/main.81d698c2.chunk.css"
+  },
+  {
+    "revision": "2fb26e57681765110c79",
+    "url": "/static/js/main.1f97e855.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "84559f50758e8ecec2b7",
+    "url": "/static/js/2.f9464edc.chunk.js"
+  },
+  {
+    "revision": "fdc91379891843d8a724ae06a7d93ee1",
+    "url": "/index.html"
+  }
+];
